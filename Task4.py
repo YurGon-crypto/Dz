83128160dd10d3f0902bf177a48342cfbdd10341
YurGon-crypto <118386859+YurGon-crypto@users.noreply.github.com>
2023-12-07 11:25:21 +0300

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,6 +1,15 @@
 class CustomException(Exception):
-    def __init__(self, msg):
-        super().__init__(msg)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        msg = args[0] if args else kwargs.get('msg', '')
         with open('logs.txt', 'a') as file:
             file.write(msg + '\n')
+
+try:
+
+    raise CustomException("Це повідомлення буде додано до logs.txt")
+
+except CustomException as e:
+    print(f"Помилка: {e}")
+   
 
